fe87cc02c571bf217db5e93da18f20987de45d4e
liuqinghang <2326713669@qq.com>
2022-01-20 22:51:43 +0800

    Initial commit

---
+++ src/main/java/com/work/university/PaperGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.work.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperGenerateApplication.class, args);
+    }
+
+}
